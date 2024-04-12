1ba3be14f38471c306b26ea2b09e201a6297c80f
Andreas Wüst <andreas.wuest.freelancer@gmail.com>
2024-04-12 20:56:14 +0200

    updated esslinger zeitung;

--- src/main/java/de/jos/freepaper/service/pages/EsslingerZeitung.java
+++ src/main/java/de/jos/freepaper/service/pages/EsslingerZeitung.java
@@ -25,6 +25,7 @@ public class EsslingerZeitung extends SupportedPage {
     public List<String> getRemovableElements() {
         final List<String> elements = super.getRemovableElements();
         elements.add("link[rel*='']");
+        elements.add("link[href*='']");
         elements.add("style");
         elements.add("script");
         elements.add("figure");
