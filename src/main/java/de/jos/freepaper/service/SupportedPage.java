2c42757c9d263844b18c3f62733e6b9efa3b2b3e
Andreas Wüst <andreas.wuest.freelancer@gmail.com>
2024-05-22 19:37:35 +0200

    removed duplicate iframe / added figure;

--- src/main/java/de/jos/freepaper/service/SupportedPage.java
+++ src/main/java/de/jos/freepaper/service/SupportedPage.java
@@ -36,7 +36,7 @@ public abstract class SupportedPage {
                 "iframe",
                 "form",
                 "noscript",
-                "iframe",
+                "figure",
                 "button"
         ));
     }
