7cce6f494ca7a9ff87b1fdf68d5cc79b79f2e93d
Andreas Wüst <andreas.wuest.freelancer@gmail.com>
2024-10-30 13:59:40 +0100

    updated focus;

--- src/test/java/de/jos/freepaper/service/pages/FocusTest.java
+++ src/test/java/de/jos/freepaper/service/pages/FocusTest.java
@@ -2,21 +2,10 @@ package de.jos.freepaper.service.pages;
 
 import org.junit.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 /**
  * @author awuest
  */
 public class FocusTest extends AbstractPageTest {
-
-//    @Test
-//    public void getPrintUrl_shouldReturnPrintUrl() {
-//        final Focus page = new Focus();
-//        final String printUrl = page.getPrintURL("http://www.focus.de/finanzen/recht/betrugs-prozess-richter-verzweifeln-an-s-k_id_5979755.html");
-//        final String expectedUrl = "http://www.focus.de/finanzen/recht/betrugs-prozess-richter-verzweifeln-an-s-k_id_5979755.html?drucken=1";
-//
-//        assertThat(printUrl).isEqualTo(expectedUrl);
-//    }
 
     @Test
     public void getDownload() {
